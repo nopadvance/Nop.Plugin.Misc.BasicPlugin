b2b390bf70a3006a703ba0dae9d4a9955f341b3f
nopadvance <53564650+nopadvance@users.noreply.github.com>
2019-08-01 16:40:10 +0530

    Add files via upload

---
+++ BasicPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.BasicPlugin
+{
+    public class BasicPlugin : BasePlugin
+    {
+        public override void Install()
+        {
+            //Put your default installation settings here
+
+            base.Install();
+        }
+        public override void Uninstall()
+        {
+            //Remove your default installation settings here
+
+            base.Uninstall();
+        }
+    }
+}
